d9309beaf3adb0a02f2003040e2481321c2ec34f
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-15 20:20:41 +0530

    created  location.ts

---
+++ src/app/shared/services/location/location.ts
@@ -0,0 +1,12 @@
+export interface GeoPoint{
+    latitude?: string;
+    longitude?: string;
+}
+
+export interface Location {
+    id?: string;
+    locationName?: string;
+    description?: string;
+    loc?: GeoPoint;
+    verified?: boolean;
+}
