269e344e625c5ac6b32d541de500556e17e7c37b
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-15 15:58:11 +0530

    created tourist profile page

---
+++ src/app/pages/tourist/tourist-profile/tourist-profile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tourist-profile',
+  templateUrl: './tourist-profile.page.html',
+  styleUrls: ['./tourist-profile.page.scss'],
+})
+export class TouristProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
