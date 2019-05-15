94c0157d142d345fac13459aa8f0dd6a273da2b2
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-15 16:00:30 +0530

    user role subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './shared/services/auth.service';
+import { User } from './shared/services/user/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
  
-  role="guide";
+  user: User = {
+    uid: '',
+    displayName: '',
+    email: '',
+    emailVerified: false,
+    photoURL: '',
+    roles: {
+      guide: false,
+      tourist: false,
+    }
+  };
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public authService: AuthService
   ) {
     this.initializeApp();
   }
@@ -24,5 +37,26 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  isLogged(): boolean {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user !== null) ? true : false;
+  }
+
+  ngOnInit() {
+    this.authService.getUser().subscribe(usr => {
+      if (usr) {
+        localStorage.setItem('user', JSON.stringify(usr));
+        this.user.roles.guide = usr.roles.guide;
+        this.user.displayName = usr.displayName;
+        this.user.emailVerified = usr.emailVerified;
+        this.user.email = usr.email;
+        this.user.uid = usr.uid;
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    localStorage.removeItem('user');
   }
 }
