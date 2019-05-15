49e071bf0348273dce38b647a8f9bec8b1350b26
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-15 16:02:33 +0530

    changd router links

--- src/app/pages/common/dashboard/dashboard.page.ts
+++ src/app/pages/common/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'src/app/shared/services/user/user';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  role="guide";
-  constructor() { }
+  user: User = {
+    uid: '',
+    displayName: '',
+    email: '',
+    emailVerified: false,
+    photoURL: '',
+    roles: {
+      guide: false,
+      tourist: false,
+    }
+  };
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  isLogged(): boolean {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user !== null) ? true : false;
+  }
 
   ngOnInit() {
+    this.authService.getUser().subscribe(usr => {
+      if (usr) {
+        localStorage.setItem('user', JSON.stringify(usr));
+        this.user.roles.guide = usr.roles.guide;
+        this.user.roles.tourist = usr.roles.tourist;
+        this.user.displayName = usr.displayName;
+        this.user.emailVerified = usr.emailVerified;
+        this.user.email = usr.email;
+        this.user.uid = usr.uid;
+      }
+    });
   }
 
 }
