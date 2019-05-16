ded7e2762f1766d6b2610549ba7f4ff61a248015
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-16 19:22:38 +0530

    created view rate component

---
+++ src/app/pages/tourist/view-rate/view-rate.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ViewRatePage } from './view-rate.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewRatePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ViewRatePage]
+})
+export class ViewRatePageModule {}
