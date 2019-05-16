647b4b03d749168740d1bac347e3dd831f30dd68
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-16 19:21:58 +0530

    created view place component

---
+++ src/app/pages/tourist/view-place/view-place.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-place',
+  templateUrl: './view-place.page.html',
+  styleUrls: ['./view-place.page.scss'],
+})
+export class ViewPlacePage implements OnInit {
+  title="Dux | Location";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
