835c77ed292d8c153774851fa7048db9274aa4cc
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-16 19:20:43 +0530

    created view package component

---
+++ src/app/pages/tourist/view-package/view-package.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-package',
+  templateUrl: './view-package.page.html',
+  styleUrls: ['./view-package.page.scss'],
+})
+export class ViewPackagePage implements OnInit {
+  title="Dux | Package";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
