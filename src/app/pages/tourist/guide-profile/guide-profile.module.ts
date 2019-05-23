a16b8afdca9f6f8104dc2d8cce7a1fa509eb521c
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-05-23 23:50:16 +0530

    created page for guide profile view for tourist

---
+++ src/app/pages/tourist/guide-profile/guide-profile.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { GuideProfilePage } from './guide-profile.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: GuideProfilePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [GuideProfilePage]
+})
+export class GuideProfilePageModule {}
